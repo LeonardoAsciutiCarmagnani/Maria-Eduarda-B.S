60ae3ccfdbd23f713172ae22c003fb81cafbad76
Leonardo Asciuti Carmagnani <lasciuti.c@gmail.com>
2023-04-15 15:42:04 -0300

    s

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-window.onscroll = function() {scrollar()};
+window.onscroll = function () { scrollar() };
 
 function scrollar() {
   if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
     document.getElementById("voltar-ao-topo").classList.remove("esconder");
+    document.getElementById("voltar-ao-topo").classList.add("my-0.1");
   } else {
     document.getElementById("voltar-ao-topo").classList.add("esconder");
+    document.getElementById("voltar-ao-topo").classList.remove("my-0.1");
+
   }
 }
 
 function voltarAoTopo() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-   }
-   
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
